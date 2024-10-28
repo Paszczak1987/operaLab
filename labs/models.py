14a5609518877fed897819f55dcdd30f2447128a
Paszczak1987 <lpaszko30@gmail.com>
2024-10-29 00:42:59 +0100

    bootstrap pinned laboratory model modifications view layer improved

--- labs/models.py
+++ labs/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Laboratory(models.Model):
     name = models.CharField(max_length=25)
     short_name = models.CharField(max_length=4)
+    
     location = models.CharField(max_length=100)
-    country = models.CharField(max_length=20, default="Unknown")
+    country = models.CharField(max_length=20, default="None")
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    leadership_area = models.CharField(max_length=5, default="None")
+    
     contact_email = models.EmailField()
     
     def __str__(self):
@@ -15,6 +18,15 @@ class Laboratory(models.Model):
         
     def __repr__(self):
         return self.short_name
+    
+    def formatted_address(self):
+        address_split = self.location.split(' ')
+        return {
+            'street': " ".join(address_split[:3]),
+            'city': " ".join(address_split[3:]),
+            'country': self.country,
+        }
+        
     
     class Meta:
         verbose_name_plural = "Laboratories"
--- labs/views.py
+++ labs/views.py
@@ -14,4 +14,11 @@ class LabDetailView(DetailView):
     template_name = 'labs/detail.html'
     context_object_name = 'lab'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        formatted_address = self.object.formatted_address()
+        context['street'] = formatted_address['street']
+        context['city'] = formatted_address['city']
+        context['country'] = formatted_address['country']
+        return context
     
